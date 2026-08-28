98ea86f114856c70944c2723fc8220364c1fd944
Muzammil <qazi.muzammil.dev@gmail.com>
2026-08-28 18:16:32 +0500

    Biome Issues Fixed

--- packages/webext-message/src/index.ts
+++ packages/webext-message/src/index.ts
@@ -1,66 +1,61 @@
-import { relay as rawRelay, sendViaRelay as rawSendViaRelay } from "./relay";
-import type { MessageName, ExtMessaging } from "./types";
-import { getActiveTab, getExtRuntime, getExtTabs } from "./utils";
+import { relay as rawRelay, sendViaRelay as rawSendViaRelay } from './relay'
+import type { MessageName, ExtMessaging } from './types'
+import { getActiveTab, getExtRuntime, getExtTabs } from './utils'
 
 export type {
-	ExtMessaging,
-	MessageName,
-	PortName,
-	PortsMetadata,
-	MessagesMetadata,
-	OriginContext,
-} from "./types";
+  ExtMessaging,
+  MessageName,
+  PortName,
+  PortsMetadata,
+  MessagesMetadata,
+  OriginContext,
+} from './types'
 
-export { initializeBackgroundMessaging } from "./background";
-export { listen as onMessage } from "./message";
-export { getPort, listen as onPort, onPortConnect } from "./port";
-export { startHub, broadcast, subscribe } from "./pub-sub";
+export { initializeBackgroundMessaging } from './background'
+export { listen as onMessage } from './message'
+export { getPort, listen as onPort, onPortConnect } from './port'
+export { startHub, broadcast, subscribe } from './pub-sub'
 
 /**
  * Send to Background Service Workers from Content Scripts or Extension pages.
  * extensionId is required to send from Content Script in main world
  */
-export const sendToBackground: ExtMessaging.SendFx<MessageName> = async (
-	req,
-) => {
-	return getExtRuntime().sendMessage(req.extensionId ?? null, req);
-};
+export const sendToBackground: ExtMessaging.SendFx<MessageName> = async req => {
+  return getExtRuntime().sendMessage(req.extensionId ?? null, req)
+}
 
 /**
  * Send to Content Scripts from Extension pages or Background Service Workers.
  * Defaults to active tab if no tabId provided
  */
-export const sendToContentScript: ExtMessaging.SendFx = async (req) => {
-	const tabId =
-		typeof req.tabId === "number" ? req.tabId : (await getActiveTab())?.id;
+export const sendToContentScript: ExtMessaging.SendFx = async req => {
+  const tabId = typeof req.tabId === 'number' ? req.tabId : (await getActiveTab())?.id
 
-	if (!tabId) {
-		throw new Error("No active tab found to send message to.");
-	}
+  if (!tabId) {
+    throw new Error('No active tab found to send message to.')
+  }
 
-	return getExtTabs().sendMessage(tabId, req);
-};
+  return getExtTabs().sendMessage(tabId, req)
+}
 
 /**
  * @deprecated Renamed to sendToContentScript
  */
-export const sendToActiveContentScript = sendToContentScript;
+export const sendToActiveContentScript = sendToContentScript
 
 /**
  * Any request sent to this relay gets sent to background, then emitted back as response
  */
-export const relayMessage: ExtMessaging.MessageRelayFx = (req) =>
-	rawRelay(req, sendToBackground);
+export const relayMessage: ExtMessaging.MessageRelayFx = req => rawRelay(req, sendToBackground)
 
 /**
  * @deprecated Migrated to relayMessage
  */
-export const relay = relayMessage;
+export const relay = relayMessage
 
-export const sendToBackgroundViaRelay: ExtMessaging.SendFx<MessageName> =
-	rawSendViaRelay;
+export const sendToBackgroundViaRelay: ExtMessaging.SendFx<MessageName> = rawSendViaRelay
 
 /**
  * @deprecated Migrated to sendToBackgroundViaRelay
  */
-export const sendViaRelay = sendToBackgroundViaRelay;
+export const sendViaRelay = sendToBackgroundViaRelay
